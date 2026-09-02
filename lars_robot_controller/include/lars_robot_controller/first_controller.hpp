7f6a54137b29d87b78c430dc1faeedf5dcb33cbe
Lars Stepputat <lars.stepputat@gmail.com>
2026-09-02 16:11:27 +0200

    First commit; own controller package started

---
+++ lars_robot_controller/include/lars_robot_controller/first_controller.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+
+#include <Eigen/Eigen>
+#include <controller_interface/controller_interface.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+namespace lars_robot_controller {
+
+/**
+ * Implementation of cartesian Impedance controller
+ */
+class CartesianImpedanceController : public controller_interface::ControllerInterface {
+ public:
+  using Vector7d = Eigen::Matrix<double, 7, 1>;
+  [[nodiscard]] controller_interface::InterfaceConfiguration command_interface_configuration()
+      const override;
+  [[nodiscard]] controller_interface::InterfaceConfiguration state_interface_configuration()
+      const override;
+  controller_interface::return_type update(const rclcpp::Time& time,
+                                           const rclcpp::Duration& period) override;
+  CallbackReturn on_init() override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+
+ private:
+  std::string arm_id_;
+  std::string robot_description_;
+  const int num_joints = 7;
+  Vector7d q_;
+  Vector7d initial_q_;
+  Vector7d dq_;
+  Vector7d dq_filtered_;
+  Vector7d k_gains_;
+  Vector7d d_gains_;
+  double elapsed_time_{0.0};
+  void updateJointStates();
+};
+
+}
